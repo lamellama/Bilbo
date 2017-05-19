28a95545830a2e476300d5060fcb893ed537bd3a
lamellama <matt.lamellama@gmail.com>
2017-05-19 09:42:14 +0100

    viewPager multiple fragments

--- app/src/main/java/bilbo/arunwebnerd/com/bilbo/MainActivity.java
+++ app/src/main/java/bilbo/arunwebnerd/com/bilbo/MainActivity.java
@@ -61,6 +61,28 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+	private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+				case 0: return FirstFragment.newInstance("FirstFragment, Instance 1");
+				case 1: return SecondFragment.newInstance("SecondFragment, Instance 1");
+				
+				default: return SecondFragment.newInstance("ThirdFragment, Default");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }       
+    }
 
 
     @Override
